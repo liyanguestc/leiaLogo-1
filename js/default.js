f70f796b0b07a7b8265f8d9d26cdcfbfa2051c4b
liyangv <liyang@visazure.com>
2015-01-06 14:53:08 +0800

    init leialogo

--- js/default.js
+++ js/default.js
@@ -1,75 +1,108 @@
- var windowWidth = window.innerWidth, windowHeight = window.innerHeight;
- var camera,renderer,scene;
+ var windowWidth = window.innerWidth,
+     windowHeight = window.innerHeight;
+ var camera, renderer, scene;
+ var mesh1;
+ var sizeM = 45;
+ var sizeMesh1 = sizeM;
  head.ready(function() {
-    Init();
-    animate();
+     Init();
+     animate();
  });
 
-function Init(){
-        scene = new THREE.Scene();
-  
-       //setup camera
-        camera = new LeiaCamera({
-           cameraPosition:new THREE.Vector3(_camPosition.x, _camPosition.y, _camPosition.z), 
-		   targetPosition: new THREE.Vector3(_tarPosition.x, _tarPosition.y, _tarPosition.z)
-        });
-        scene.add(camera);
- 
-  
-       //setup rendering parameter
- 		renderer = new LeiaWebGLRenderer({
-         antialias:true, 
- 		renderMode: _renderMode, 
-		shaderMode: _nShaderMode,
-		colorMode : _colorMode,
-		devicePixelRatio: 1 
-        } );
- 		renderer.Leia_setSize( {
-                          width: windowWidth,
-                         height:windowHeight,
-                         autoFit:true
-                        } );
- 		document.body.appendChild( renderer.domElement );
-  
-       //add object to Scene
-        addObjectsToScene();
-  
-        //add Light
- 		addLights();
-  
-        //add Gyro Monitor
-        //addGyroMonitor();
+ function Init() {
+     scene = new THREE.Scene();
+
+     //setup camera
+     camera = new LeiaCamera({
+         cameraPosition: new THREE.Vector3(_camPosition.x, _camPosition.y, _camPosition.z),
+         targetPosition: new THREE.Vector3(_tarPosition.x, _tarPosition.y, _tarPosition.z)
+     });
+     scene.add(camera);
+
+
+     //setup rendering parameter
+     renderer = new LeiaWebGLRenderer({
+         antialias: true,
+         renderMode: _renderMode,
+         shaderMode: _nShaderMode,
+         colorMode: _colorMode,
+         devicePixelRatio: 1
+     });
+     renderer.Leia_setSize({
+         width: windowWidth,
+         height: windowHeight,
+         autoFit: true
+     });
+     document.body.appendChild(renderer.domElement);
+
+     //add object to Scene
+     addObjectsToScene();
+
+     //add Light
+     addLights();
+
+     //add Gyro Monitor
+     //addGyroMonitor();
  }
 
- function animate() 
- {
- 	requestAnimationFrame( animate );
-    renderer.setClearColor(new THREE.Color().setRGB(1.0, 1.0, 1.0)); 
-	renderer.Leia_render({
-     scene:scene, 
-     camera:camera,
-     holoScreenSize:_holoScreenSize,
-     holoCamFov:_camFov,
-      upclip: _up,
-     downclip:  _down,
-     messageFlag:_messageFlag
-   });
+ function animate() {
+     requestAnimationFrame(animate);
+    // renderer.setClearColor(new THREE.Color().setRGB(1.0, 1.0, 1.0));
+     renderer.Leia_render({
+         scene: scene,
+         camera: camera,
+         holoScreenSize: _holoScreenSize,
+         holoCamFov: _camFov,
+         upclip: _up,
+         downclip: _down,
+         messageFlag: _messageFlag
+     });
  }
 
-function addObjectsToScene(){
-    //Add your objects here
-    var graph = new THREE.Mesh(new THREE.SphereGeometry(8, 30, 10), new   THREE.MeshLambertMaterial({color:0xffffff}));
-	  scene.add(graph);
-}
+ function addObjectsToScene() {
+     //Add your objects here
+     // var graph = new THREE.Mesh(new THREE.SphereGeometry(8, 30, 10), new   THREE.MeshLambertMaterial({color:0xffffff}));
+     // scene.add(graph);
+     readSTLs('resource/leialogo.stl', '', '');
+ }
 
-function addLights(){
-    //Add Lights Here
-    var xl = new THREE.DirectionalLight( 0x555555 );
- 	xl.position.set( 1, 0, 2 );
- 	scene.add( xl );
- 	var pl = new THREE.PointLight(0x111111);
- 	pl.position.set(-20, 10, 20);
- 	scene.add(pl);
- 	var ambientLight = new THREE.AmbientLight(0x111111);	
- 	scene.add(ambientLight);
-}
+ function addLights() {
+     //Add Lights Here
+     var xl = new THREE.DirectionalLight(0x555555);
+     xl.position.set(1, 0, 2);
+     scene.add(xl);
+     var pl = new THREE.PointLight(0x111111);
+     pl.position.set(-20, 10, 20);
+     scene.add(pl);
+     var ambientLight = new THREE.AmbientLight(0x111111);
+     scene.add(ambientLight);
+
+
+ }
+
+ function readSTLs(filename1, filename2, filename3) {
+     var xhr1 = new XMLHttpRequest();
+     xhr1.onreadystatechange = function() {
+         if (xhr1.readyState == 4) {
+             if (xhr1.status == 200 || xhr1.status === 0) {
+                 var rep = xhr1.response;
+
+                 mesh1 = parseStlBinary(rep, 0xffffff);
+                 mesh1.material.side = THREE.DoubleSide;
+                 mesh1.castShadow = true;
+                 mesh1.receiveShadow = true;
+                 mesh1.material.metal = true;
+
+                 mesh1.scale.set(sizeMesh1, sizeMesh1, sizeMesh1);
+                 scene.add(mesh1);
+                 newMeshReady = true;
+             }
+         }
+     };
+     xhr1.onerror = function(e) {
+         console.log(e);
+     };
+     xhr1.open("GET", filename1, true);
+     xhr1.responseType = "arraybuffer";
+     xhr1.send(null);
+ }
