34bbeb691b3370aa058146f87eca9f984d74bfed
liyanguestc <liyanguestc@gmail.com>
2015-01-12 11:17:39 +0800

    holoide commit

--- js/default.js
+++ js/default.js
@@ -51,21 +51,17 @@
 
  function animate() {
      requestAnimationFrame(animate);
-    // renderer.setClearColor(new THREE.Color().setRGB(1.0, 1.0, 1.0));
 
-     for(var i = 0;i < meshArray.length;i++){
-       var curMeshGroup = meshArray[i].meshGroup;
-       switch(meshArray[i].name){
-        // case 'Cube':
-          // curMesh.rotation.set(0.2 * Math.sin(3.2 * LEIA.time), 0 * Math.PI / 2, 0.25 * Math.sin(4 * LEIA.time));
-        //   break;
-          case 'TheTip':
-            curMeshGroup.rotation.set(0,  0, Math.PI / 2 * LEIA.time);
-            break;
-         default:
-           curMeshGroup.rotation.set(0,  0, Math.PI / 2 * LEIA.time);
-           break;
-       }
+     for (var i = 0; i < meshArray.length; i++) {
+         var curMeshGroup = meshArray[i].meshGroup;
+         switch (meshArray[i].name) {
+             case 'TheTip':
+               //  curMeshGroup.rotation.set(0, 0, Math.PI / 2 * LEIA.time);
+                 break;
+             default:
+             //    curMeshGroup.rotation.set(0, 0, Math.PI / 2 * LEIA.time);
+                 break;
+         }
      }
      renderer.Leia_render({
          scene: scene,
@@ -79,85 +75,151 @@
  }
 
  function addObjectsToScene() {
+
+     //Add your objects here
+     addSTLModel({
+         path: 'resource/Cube.stl',
+         meshGroupName: 'Cube',
+         meshSizeX: 30,
+         meshSizeY: 30,
+         meshSizeZ: 30,
+         translateX: 0,
+         translateY: 0,
+         translateZ: 0,
+     });
+
+     addSTLModel({
+         path: 'resource/SmallerSquares.stl',
+         meshGroupName: 'SmallerSquares',
+         meshSizeX: 30,
+         meshSizeY: 30,
+         meshSizeZ: 30,
+         translateX: 0,
+         translateY: 0,
+         translateZ: 0,
+     });
+
+     addSTLModel({
+         path: 'resource/SmallestSquares.stl',
+         meshGroupName: 'SmallestSquares',
+         meshSizeX: 30,
+         meshSizeY: 35,
+         meshSizeZ: 30,
+         translateX: 0,
+         translateY: 0,
+         translateZ: 0,
+     });
+
+     addSTLModel({
+         path: 'resource/TheTip.stl',
+         meshGroupName: 'TheTip',
+         meshSizeX: 5,
+         meshSizeY: 5,
+         meshSizeZ: 5,
+         translateX: 0,
+         translateY: 22,
+         translateZ: 0,
+     });
    
-   //Add your objects here
-   addSTLModel({
-     path: 'resource/Cube.stl',
-     meshGroupName: 'Cube',
-     meshSizeX: 30,
-     meshSizeY: 30,
-     meshSizeZ: 30,
-     translateX: 0,
-     translateY: 0,
-     translateZ: 0,
-   });
+   //add Text
+    addTextMenu({
+      text: "Menu 1",
+      name: "menu1",
+      positionX: -5,
+      positionY: 0,
+      positionZ: 13
+    });
    
-   addSTLModel({
-     path: 'resource/SmallerSquares.stl',
-     meshGroupName: 'SmallerSquares',
-     meshSizeX: 30,
-     meshSizeY: 30,
-     meshSizeZ: 30,
-     translateX: 0,
-     translateY: 0,
-     translateZ: 0,
-   });
-   
-    addSTLModel({
-     path: 'resource/SmallestSquares.stl',
-     meshGroupName: 'SmallestSquares',
-     meshSizeX: 30,
-     meshSizeY: 35,
-     meshSizeZ: 30,
-     translateX: 0,
-     translateY: 0,
-     translateZ: 0,
-   });
-   
-   addSTLModel({
-     path: 'resource/TheTip.stl',
-     meshGroupName: 'TheTip',
-     meshSizeX: 5,
-     meshSizeY: 5,
-     meshSizeZ: 5,
-     translateX: 0,
-     translateY: 22,
-     translateZ: 0,
-   });
-   
-   //  LEIA_setBackgroundPlane('resource/brickwall_900x600_small.jpg');
+    addTextMenu({
+      text: "Menu 2",
+      name: "menu2",
+      positionX: -5,
+      positionY: 0,
+      positionZ: -13
+      
+    });
+
+     //  LEIA_setBackgroundPlane('resource/brickwall_900x600_small.jpg');
  }
 
  function addLights() {
      //Add Lights Here
      var light = new THREE.SpotLight(0xffffff);
-    //light.color.setHSL( Math.random(), 1, 0.5 );
-    light.position.set(0, 100, 200);
-    light.shadowCameraVisible = true;
-    light.castShadow = true;
-    light.shadowMapWidth = light.shadowMapHeight = 256;
-    light.shadowDarkness = 0.7;
-    scene.add(light);
+     //light.color.setHSL( Math.random(), 1, 0.5 );
+     light.position.set(0, 100, 200);
+     light.shadowCameraVisible = true;
+     light.castShadow = true;
+     light.shadowMapWidth = light.shadowMapHeight = 256;
+     light.shadowDarkness = 0.7;
+     scene.add(light);
 
-    var ambientLight = new THREE.AmbientLight(0x222222);
-    scene.add(ambientLight);
+     var ambientLight = new THREE.AmbientLight(0x222222);
+     scene.add(ambientLight);
  }
 
-function addSTLModel(parameters){//(filename, meshName, meshSize) {
+ function addTextMenu(parameters){
+    parameters = parameters || {};
+   
+   var strText = parameters.text;
+   var posX = parameters.positionX;
+   var posY = parameters.positionY;
+   var posZ = parameters.positionZ;
+   var name = parameters.name;
+   if(posX === undefined || posY === undefined || posZ === undefined){
+     posX = 0;
+     posY = 0;
+     posZ = 0;
+   }
+   var menuGeometry = new THREE.TextGeometry(
+        strText, {
+            size: 3,
+            height: 1,
+            curveSegments: 4,
+            font: "helvetiker",
+            weight: "normal",
+            style: "normal",
+            bevelThickness: 0.5,
+            bevelSize: 0.25,
+            bevelEnabled: true,
+            material: 0,
+            extrudeMaterial: 1
+        }
+    ); 
+    var menuMaterial = new THREE.MeshFaceMaterial(
+        [
+            new THREE.MeshPhongMaterial({
+                color: 0xffffff,
+                shading: THREE.FlatShading
+            }), // front
+            new THREE.MeshPhongMaterial({
+                color: 0xffffff,
+                shading: THREE.SmoothShading
+            }) // side
+        ]
+    );
+    var menuMesh = new THREE.Mesh(menuGeometry, menuMaterial);
+    menuMesh.position.set(posX, posY, posZ);
+    var group = new THREE.Object3D();
+    group.add(menuMesh);
+    scene.add(group);
+    meshArray.push({meshGroup:group,name:"menu1"});
+   
+ }
+ function addSTLModel(parameters) { //(filename, meshName, meshSize) {
      parameters = parameters || {};
      var path = parameters.path;
      var meshSizeX = parameters.meshSizeX;
-    var meshSizeY = parameters.meshSizeY;
-    var meshSizeZ = parameters.meshSizeZ;
-    var tx =  parameters.translateX;
-    var ty =  parameters.translateY;
-    var tz =  parameters.translateZ;
-    var meshName = parameters.meshGroupName;
-    if(parameters.meshSizeX === undefined || parameters.meshSizeY === undefined  || parameters.meshSizeZ === undefined ){
-      meshSizeX = 1;
-      meshSizeY = 1;
-      meshSizeZ = 1;
-    }
+     var meshSizeY = parameters.meshSizeY;
+     var meshSizeZ = parameters.meshSizeZ;
+     var tx = parameters.translateX;
+     var ty = parameters.translateY;
+     var tz = parameters.translateZ;
+     var meshName = parameters.meshGroupName;
+     if (parameters.meshSizeX === undefined || parameters.meshSizeY === undefined || parameters.meshSizeZ === undefined) {
+         meshSizeX = 1;
+         meshSizeY = 1;
+         meshSizeZ = 1;
+     }
      var xhr1 = new XMLHttpRequest();
      xhr1.onreadystatechange = function() {
          if (xhr1.readyState == 4) {
@@ -169,14 +231,17 @@ function addSTLModel(parameters){//(filename, meshName, meshSize) {
                  mesh1.castShadow = true;
                  mesh1.receiveShadow = true;
                  mesh1.material.metal = true;
-        
+
                  mesh1.scale.set(meshSizeX, meshSizeY, meshSizeZ);
                  mesh1.position.set(tx, ty, tz);
                  var group = new THREE.Object3D();
                  group.add(mesh1);
                  scene.add(group);
-                 meshArray.push({meshGroup:group,name:meshName});
-                // newMeshReady = true;
+                 meshArray.push({
+                     meshGroup: group,
+                     name: meshName
+                 });
+                 // newMeshReady = true;
              }
          }
      };
@@ -188,20 +253,20 @@ function addSTLModel(parameters){//(filename, meshName, meshSize) {
      xhr1.send(null);
  }
 
-function LEIA_setBackgroundPlane(filename, aspect) {
-    var foregroundPlaneTexture = new THREE.ImageUtils.loadTexture(filename);
-    foregroundPlaneTexture.wrapS = foregroundPlaneTexture.wrapT = THREE.RepeatWrapping;
-    foregroundPlaneTexture.repeat.set(1, 1);
+ function LEIA_setBackgroundPlane(filename, aspect) {
+     var foregroundPlaneTexture = new THREE.ImageUtils.loadTexture(filename);
+     foregroundPlaneTexture.wrapS = foregroundPlaneTexture.wrapT = THREE.RepeatWrapping;
+     foregroundPlaneTexture.repeat.set(1, 1);
 
-    //
-    var planeMaterial = new THREE.MeshPhongMaterial({
-        map: foregroundPlaneTexture,
-        color: 0xffdd99
-    });
-    var planeGeometry = new THREE.PlaneGeometry(80, 60, 10, 10);
-    plane = new THREE.Mesh(planeGeometry, planeMaterial);
-    plane.position.z = -6;
-    plane.castShadow = false;
-    plane.receiveShadow = true;
-    scene.add(plane);
-}
+     //
+     var planeMaterial = new THREE.MeshPhongMaterial({
+         map: foregroundPlaneTexture,
+         color: 0xffdd99
+     });
+     var planeGeometry = new THREE.PlaneGeometry(80, 60, 10, 10);
+     plane = new THREE.Mesh(planeGeometry, planeMaterial);
+     plane.position.z = -6;
+     plane.castShadow = false;
+     plane.receiveShadow = true;
+     scene.add(plane);
+ }
